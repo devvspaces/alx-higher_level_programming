cb2d11e2fb359bdd69e68c80a9930e576cc53091
Netrobe Ubuntu <netrobepy@gmail.com>
2023-06-08 15:30:00 +0100

    debug calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,28 +1,28 @@
 #!/usr/bin/python3
 
 if __name__ == '__main__':
-    from sys import argv, exit
+    import sys
     from calculator_1 import add, sub, mul, div
 
-    argc = len(argv) - 1
+    argc = len(sys.argv) - 1
 
     if argc != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
+        sys.exit(1)
 
-    if argv[2] not in "+*-/":
+    if sys.argv[2] not in "+*-/":
         print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
+        sys.exit(1)
 
-    a = int(argv[1])
-    op = argv[2]
-    b = int(argv[3])
+    a = int(sys.argv[1])
+    op = sys.argv[2]
+    b = int(sys.argv[3])
 
     ops = {
-        "+": lambda a, b: print("{} + {} = {}".format(a, b, add(a, b))),
-        "-": lambda a, b: print("{} - {} = {}".format(a, b, sub(a, b))),
-        "*": lambda a, b: print("{} * {} = {}".format(a, b, mul(a, b))),
-        "/": lambda a, b: print("{} / {} = {}".format(a, b, div(a, b))),
+        "+": add,
+        "-": sub,
+        "*": mul,
+        "/": div,
     }
 
-    ops.get(op)(a, b)
+    print("{} {} {} = {}".format(a, op, b, ops.get(op)(a, b)))
