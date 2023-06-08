eb7d4ebec51835ffa2df4c7c3c28a1f50126072f
Netrobe Ubuntu <netrobepy@gmail.com>
2023-06-08 15:25:05 +0100

    update

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == '__main__':
-    from sys import argv
+    from sys import argv, exit
     from calculator_1 import add, sub, mul, div
 
     argc = len(argv) - 1
