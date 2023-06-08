fd05dbe0c3b5a735ad3df0dc16dd8c6856988a5a
Netrobe Ubuntu <netrobepy@gmail.com>
2023-06-08 15:31:35 +0100

    update

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,28 +1,19 @@
 #!/usr/bin/python3
 
-if __name__ == '__main__':
-    import sys
+if __name__ == "__main__":
+    """Handle basic arithmetic operations."""
     from calculator_1 import add, sub, mul, div
+    import sys
 
-    argc = len(sys.argv) - 1
-
-    if argc != 3:
+    if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    if sys.argv[2] not in "+*-/":
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(ops.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
     a = int(sys.argv[1])
-    op = sys.argv[2]
     b = int(sys.argv[3])
-
-    ops = {
-        "+": add,
-        "-": sub,
-        "*": mul,
-        "/": div,
-    }
-
-    print("{} {} {} = {}".format(a, op, b, ops.get(op)(a, b)))
+    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
