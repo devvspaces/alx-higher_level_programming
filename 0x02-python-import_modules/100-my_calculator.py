3df7c5e0dfe0e2ce772bc31da76877fe524d57ea
Netrobe Ubuntu <netrobepy@gmail.com>
2023-06-08 15:37:33 +0100

    used exec

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,19 +1,28 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-    """Handle basic arithmetic operations."""
-    from calculator_1 import add, sub, mul, div
+if __name__ == '__main__':
     import sys
+    from calculator_1 import add, sub, mul, div
 
-    if len(sys.argv) - 1 != 3:
+    argc = len(sys.argv) - 1
+
+    if argc != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    ops = {"+": add, "-": sub, "*": mul, "/": div}
-    if sys.argv[2] not in list(ops.keys()):
+    if sys.argv[2] not in "+*-/":
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
     a = int(sys.argv[1])
+    op = sys.argv[2]
     b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
+
+    ops = {
+        "+": add,
+        "-": sub,
+        "*": mul,
+        "/": div,
+    }
+
+    print("{} {} {} = {}".format(a, op, b, ops.get(op)(a, b)))
--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,2 +1,2 @@
 #!/usr/bin/python3
-__builtins__.__dict__['prin*t'.replace('*', '')]('#pythoniscool')
+exec("print('#pythoniscool')")
