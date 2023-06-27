d6752bfc4df81d8cedcef9aab123e37ab63d1c99
Netrobe Ubuntu <netrobepy@gmail.com>
2023-06-27 18:15:58 +0100

    updated

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,6 +4,7 @@ def safe_print_list(my_list=[], x=0):
     """
     Print all the items in my_list safely
     """
+    i = -1
     for i in range(x):
         try:
             print(my_list[i], end="")
@@ -18,7 +19,7 @@ def safe_print_list(my_list=[], x=0):
 if __name__ == "__main__":
     my_list = [1, 2, 3, 4, 5]
 
-    nb_print = safe_print_list(my_list, 2)
+    nb_print = safe_print_list(my_list, 0)
     print("nb_print: {:d}".format(nb_print))
     nb_print = safe_print_list(my_list, len(my_list))
     print("nb_print: {:d}".format(nb_print))
