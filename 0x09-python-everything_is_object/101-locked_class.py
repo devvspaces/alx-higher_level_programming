78f9991886eb209c4d99fd41cc26f7dc4edaf803
Netrobe Ubuntu <netrobepy@gmail.com>
2023-07-05 12:50:27 +0100

    update

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 
+"""
+LockedClass Module
+"""
+
+
 class LockedClass:
     def __setattr__(self, name, value):
         if name != "first_name":
-            raise AttributeError("'LockedClass' object has no attribute '{}'".format(name))
+            raise AttributeError(
+                "'LockedClass' object has no attribute '{}'".format(name))
         super().__setattr__(name, value)
-
