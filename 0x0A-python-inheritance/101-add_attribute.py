2e67a6102a9e1a26e0200c6bcfcb947a9701a4d0
Netrobe Ubuntu <netrobepy@gmail.com>
2023-07-11 05:57:54 +0100

    update

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+Add attribute
+"""
+
+
+def validate(obj):
+    """
+    Check if obj is immutable
+    """
+    if type(obj) in [tuple, str, int, frozenset]:
+        raise TypeError("can't add new attribute")
+
+
+def add_attribute(obj, name, value):
+    """
+    Add new attribute to obj
+    """
+    slots = getattr(obj, "__slots__", None)
+    if slots is None:
+        validate(obj)
+        if getattr(obj, name, None) is None:
+            raise TypeError("can't add new attribute")
+
+    setattr(obj, name, value)
