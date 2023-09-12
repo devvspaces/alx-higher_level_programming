baf08211d84c8fa22f3e6d5b628670c4a6d179e5
Netrobe Ubuntu <netrobepy@gmail.com>
2023-09-12 05:20:22 +0100

    update

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+function factorial(num) {
+  if (num === 0) {
+    return 1;
+  }
+  return num * factorial(num - 1);
+}
+
+const arg1 = process.argv[2];
+let num = parseInt(arg1, 10);
+
+if (Number.isNaN(num)) {
+  num = 0;
+}
+
+console.log(factorial(num));
---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+
+if (args.length <= 1) {
+  console.log(0);
+  process.exit(0);
+}
+
+for (let i = 0; i < args.length; i += 1) {
+  args[i] = parseInt(args[i], 10);
+}
+
+args.sort();
+console.log(args[args.length - 2]);
