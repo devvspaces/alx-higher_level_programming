c466df4a860228c7d659a4c483c77add1cd49863
Netrobe Ubuntu <netrobepy@gmail.com>
2023-09-12 04:46:02 +0100

    Working

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+if (args.length === 0) {
+  console.log('No argument');
+}
+
+if (args.length === 1) {
+  console.log('Argument found');
+}
+
+if (args.length > 1) {
+  console.log('Arguments found');
+}
