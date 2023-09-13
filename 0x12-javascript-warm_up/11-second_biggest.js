3a6fa11dacb88dccb0904b1efd17214b9c5cf88b
Netrobe Ubuntu <netrobepy@gmail.com>
2023-09-13 03:19:43 +0100

    Resolved failing tasks

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -11,5 +11,5 @@ for (let i = 0; i < args.length; i += 1) {
   args[i] = parseInt(args[i], 10);
 }
 
-args.sort();
+args.sort((a, b) => a - b);
 console.log(args[args.length - 2]);
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 const arg = process.argv[2];
+const size = parseInt(arg, 10);
 
-if (!arg || Number.isNaN(arg)) {
+if (!arg || Number.isNaN(size)) {
   console.log('Missing size');
 } else {
-  const size = parseInt(arg, 10);
   for (let x = 0; x < size; x += 1) {
     let row = '';
     for (let y = 0; y < size; y += 1) {
