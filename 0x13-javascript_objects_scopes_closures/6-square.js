7c2acbdb309d77234709d2f68ec91fa8244e5c86
Netrobe Ubuntu <netrobepy@gmail.com>
2023-09-13 05:57:18 +0100

    update

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
 
-const Rectangle = require('./4-rectangle');
+const Parent = require('./5-square');
 
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+class Square extends Parent {
   charPrint (c) {
     const char = c ?? 'X';
     let row = '';
