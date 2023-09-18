41d24ce60e455cd2d31abfa9c31f2a40443c67b0
Netrobe Ubuntu <netrobepy@gmail.com>
2023-09-18 06:50:12 +0100

    update

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -6,7 +6,7 @@ City objects contained in the DB
 import sys
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, joinedload
 
 from relationship_city import City
 from relationship_state import Base, State
@@ -24,10 +24,10 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State).outerjoin(
-        City).order_by(State.id, City.id).all()
-
-    for state in states:
+    for state in session.query(State)\
+        .options(joinedload(State.cities))\
+            .all():
         print("{}: {}".format(state.id, state.name))
         for city in state.cities:
-            print("    {}: {}".format(city.id, city.name))
+            print("\t{}: {}".format(city.id, city.name))
+    session.close()
--- 0x0F-python-object_relational_mapping/test.py
+++ 0x0F-python-object_relational_mapping/test.py
@@ -6,31 +6,24 @@ that contains an 'a' in the name from supplied db
 from sys import argv
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.orm import sessionmaker, selectinload, joinedload, subqueryload
 
-from model_state import Base, State, Column, Integer, String
-from sqlalchemy import ForeignKey
-
-
-class City(Base):
-    __tablename__ = 'cities'
-
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
-    name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+from relationship_city import Base, State, City
 
 
 if __name__ == "__main__":
     db_url = "mysql+mysqldb://{}:{}@127.0.0.1:3306/{}".format(
         argv[1], argv[2], argv[3])
-    engine = create_engine(db_url)
+    engine = create_engine(db_url, echo=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for city, state in session.query(
-        City, State
-    ).filter(City.state_id==State.id).order_by(City.id).all():
-        print("{}: ({}) {}".format(state.name, city.id, city.name))
+    for state in session.query(State)\
+        .options(joinedload(State.cities))\
+            .all():
+        print("{}: {}".format(state.id, state.name))
+        for city in state.cities:
+            print("\t{}: {}".format(city.id, city.name))
 
     session.close()
