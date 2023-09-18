3b8d47d156ccf2e4990895a68d47ff5b92b28ee5
Netrobe Ubuntu <netrobepy@gmail.com>
2023-09-18 05:58:20 +0100

    update

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -6,6 +6,7 @@ to work with MySQLAlchemy ORM.
 
 from relationship_state import Base, State
 from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(Base):
@@ -15,3 +16,4 @@ class City(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state = relationship("State", back_populates='cities')
--- 0x0F-python-object_relational_mapping/test.py
+++ 0x0F-python-object_relational_mapping/test.py
@@ -6,22 +6,29 @@ that contains an 'a' in the name from supplied db
 from sys import argv
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 
-from model_state import Base, State
+from model_state import Base, State, Column, Integer, String
+from sqlalchemy import ForeignKey
+
+
+class City(Base):
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+
 
 if __name__ == "__main__":
-    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+    db_url = "mysql+mysqldb://{}:{}@127.0.0.1:3306/{}".format(
         argv[1], argv[2], argv[3])
-    engine = create_engine(db_url, echo=True)
+    engine = create_engine(db_url)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # state = State(name="Louisiana")
-    # session.add(state)
+    for row in session.query(City, State).all():
+        print(row)
 
-    # session.query(State).filter(State.id == 2).update({'name': 'New Mexico'})
-
-    session.commit()
     session.close()
