53cecd5052161e2b54114cb5949d9985db6ad556
Netrobe Ubuntu <netrobepy@gmail.com>
2023-10-03 04:06:14 +0100

    Made changes

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 # A Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
 
-STATUS="$(curl -s -o /dev/null -w "%{http_code}" "$1")"
-
-if [ "$STATUS" -eq 200 ]
-then
-  curl -s "$1"
-fi
+curl -s -L "$1" 200
