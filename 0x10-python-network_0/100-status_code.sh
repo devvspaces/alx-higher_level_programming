e61f8cb8a2ee41764eec7952f85e5018388a92f3
Netrobe Ubuntu <netrobepy@gmail.com>
2023-10-03 04:39:20 +0100

    Made changes

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -s -I "$1" | grep "HTTP/*" | cut -d " " -f2
+curl -s -o /dev/null -w "%{http_code}" "$1"
