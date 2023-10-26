333ee58039bcbbaaa00199bcb4104db279377fc1
Netrobe Ubuntu <netrobepy@gmail.com>
2023-10-26 05:33:55 +0100

    update

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const fs = require("fs");
+
+const handler = (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data.toString());
+  }
+};
+
+fs.readFile(process.argv[2], "utf-8", handler);
